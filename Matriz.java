3e3b8e33d92f40278a2d75ff579bcea379529908
Gabriel-Shallon <shallonhatake@gmail.com>
2022-09-29 08:16:41 -0400

    Update Matriz.java

--- Matriz.java
+++ Matriz.java
@@ -4,8 +4,10 @@ public class Matriz {
     
     public static void main(String[] args){
 
-int[][] matA = new int[3][3];
+int c = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre com o números de colunas"));
+int l = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre com o números de colunas"));
 
+int matA[][] = new int[c][l];
 
 for (int j = 0; j<matA.length; j++){
      for (int i = 0; i<matA.length; i++){
