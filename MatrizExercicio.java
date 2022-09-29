110b0dd376a4174145ca222f1e0372ed9d43baaa
Gabriel-Shallon <shallonhatake@gmail.com>
2022-09-29 09:24:51 -0400

    Create MatrizExercicio.java

---
+++ MatrizExercicio.java
@@ -0,0 +1,54 @@
+import javax.swing.JOptionPane;
+
+public class MatrizExercicio {
+    
+    public static void main(String[] args){
+
+
+        int c = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre com o números de colunas"));
+        int l = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre com o números de linhas"));
+        
+        int matA[][] = new int[c][l];
+        
+        for (int j = 0; j<matA.length; j++){
+             for (int i = 0; i<matA.length; i++){
+        
+                matA[j][i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre com o valor."));
+        
+            }
+        }
+        
+        
+
+        for (int j = 0; j<matA.length; j++){
+        
+            for (int i = 0; i<matA.length; i++){
+        
+                JOptionPane.showMessageDialog(null, matA[j][i]);
+        
+            }
+        }
+
+        for (int j = 0; j<matA.length; j++){
+        
+            for (int i = 0; i<matA.length; i++){
+        
+                if (matA[j][i]<0){
+
+                    JOptionPane.showMessageDialog(null, matA[j][i]+" é negativo");
+
+                }
+            }
+        }
+
+        int i = 0;
+        for (int j = 0; j<matA.length; j++){
+        i =+ j;
+                
+              JOptionPane.showMessageDialog(null, matA[j][i]+" faz parte da diagonal principal.");
+       
+           }
+       }
+
+    }
+
