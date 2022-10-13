0092295e1c7016e137fdfe2f35859d0d0ca1a05d
Gabriel-Shallon <shallonhatake@gmail.com>
2022-10-13 07:59:45 -0400

    Create Aula13_10_22.java

---
+++ Aula13_10_22.java
@@ -0,0 +1,44 @@
+import javax.swing.JOptionPane;
+
+public class Aula13_10_22 {
+    
+    public static void main(String[] args){
+
+        int c = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre com o números de colunas"));
+        int l = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre com o números de linhas"));
+        
+        int matA[][] = new int[c][l];
+        int vetA[] = new int[c];
+        
+        for (int j = 0; j<matA.length; j++){
+            for (int i = 0; i<matA.length; i++){
+       
+               matA[j][i] = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre com o valor da Matriz A["+j+"]["+i+"]"));
+       
+           }
+       }
+
+       
+       for (int j = 0; j<matA.length; j++){
+        
+        for (int i = 0; i<matA.length; i++){
+    
+            if (matA[j][i]>vetA[j]){
+                
+           vetA[j] = matA[j][i];
+
+             }
+          }
+        }
+    
+    for (int i = 0; i < matA.length; i++){
+
+
+        JOptionPane.showMessageDialog(null, "O maior número da linha "+i+" é "+vetA[i]+".");
+
+
+    }
+    
+    
+    } 
+}
